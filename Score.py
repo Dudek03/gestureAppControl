aed1d5c36d0222986e2feb40c2a776e179deedf4
Tomek865 <tomek.malinowski03@gmail.com>
2026-01-14 15:34:55 +0100

    add Score class

---
+++ Score.py
@@ -0,0 +1,20 @@
+import pg as pygame
+
+class Score:
+    def __init__(self):
+        self.left_score = 0
+        self.right_score = 0
+        self.font = pygame.font.Font(None,100)
+
+    def add_point_left(self):
+        self.left_score += 1
+
+    def add_point_right(self):
+        self.right_score += 1
+        
+    def reset_scores(self):
+        self.left_score = 0
+        self.right_score = 0
+
+    def get_score(self):
+        return (self.left_score, self.right_score)
