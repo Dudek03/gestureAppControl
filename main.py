eb49c644baf5cfee3ffc85b450711391a76f6de3
Tomek865 <tomek.malinowski03@gmail.com>
2026-02-11 18:08:21 +0100

    Resizable update

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ clock = pg.time.Clock()
 running = True
 dt = 0
 
+min_screen_size = (700, 400)
+
 if __name__ == "__main__":
     game = Game(mode="normal")
     while running:
@@ -21,7 +23,8 @@ if __name__ == "__main__":
                 running = False
             if event.type == pg.VIDEORESIZE:
                 screen_size = pg.display.get_window_size()
-                Screen_helper.set_screen_size(screen_size)
+                Screen_helper.set_screen_size(((max(screen_size[0],min_screen_size[0])),(max(screen_size[1],min_screen_size[1]))))
+                screen = pg.display.set_mode(Screen_helper.get_size(), pg.RESIZABLE)
                 game.on_display_resize()
         screen.fill(UI_settings.get_screen_fill_color())
         game.update()
